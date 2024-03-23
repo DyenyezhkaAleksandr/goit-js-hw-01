4cfb3510326d6fc428812747c070e0319ca736fa
DyenyezhkaAleksandr <150829914+DyenyezhkaAleksandr@users.noreply.github.com>
2024-03-23 11:54:01 +0200

    add

--- js/makeTransaction.js
+++
@@ -1,5 +0,0 @@
-function makeTransaction(quantity, pricePerDroid) {
-    console.log("🚀 ~ makeTransaction ~ makeTransaction:", makeTransaction);
-    const quantity = 5;
-    const pricePerDroid = 3000;
-}
